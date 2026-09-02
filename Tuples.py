cfebe29558f79c18a75e590031e0077ca8ab08af
Paarth Chaudhari <paarth.chaudhari07@gmail.com>
2026-09-03 00:21:53 +0530

    Add files via upload

---
+++ Tuples.py
@@ -0,0 +1,8 @@
+point = [10, 20]
+point[0] = 50
+
+locations = {} #create a dictionary called locations that will store the coordinates of different locations as keys and their corresponding names as values
+
+locations[(2, 3)] = "Treasure" #add a key-value pair to the locations dictionary where the key is the tuple (2, 3) representing the coordinates of the location and the value is the string "Treasure" representing the name of the location
+
+coordinates = (5, 10)#create a tuple called coordinates that stores the x and y coordinates of a point in 2D space. The first element of the tuple is 5, representing the x-coordinate, and the second element is 10, representing the y-coordinate.
