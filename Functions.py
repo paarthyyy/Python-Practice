5c9f37af05a3d580dbf99c920a180981c68049d6
Paarth Chaudhari <paarth.chaudhari07@gmail.com>
2026-09-03 00:20:42 +0530

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,23 @@
+print(5 + 10)
+print(20 + 30)
+print(40 + 50)  
+
+def add(a, b): #define a function called add that takes two parameters, a and b, and returns their sum
+    return a + b
+
+print(add(5, 10))
+print(add(20, 30))
+
+def square(x): #define a function called square that takes one parameter, x, and returns the square of x
+    return x * x
+answer = square(5)
+
+print(answer)
+
+class Solution: #start of a class called Solution
+    def twoSum(self, nums, target): #define a method called twoSum that takes two parameters, nums and target
+        # Your code here
+        pass            
+    def someProblem(self, nums):
+        # Your code here
+        pass
